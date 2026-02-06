993928924006203b76a37aced0ad990c0403fc9c
Chijindu Nwokeohuru <chijindu.nwokeohuru@gmail.com>
2026-02-06 11:42:03 +0100

    feat: add edit and delete actions to the FAQ resource table

--- app/Filament/Resources/FaqResource.php
+++ app/Filament/Resources/FaqResource.php
@@ -37,6 +37,10 @@ class FaqResource extends Resource
             TextColumn::make('id')->sortable(),
             TextColumn::make('question')->searchable(),
             BooleanColumn::make('is_active'),
+        ])
+        ->actions([
+            \Filament\Actions\EditAction::make(),
+            \Filament\Actions\DeleteAction::make(),
         ]);
     }
 
