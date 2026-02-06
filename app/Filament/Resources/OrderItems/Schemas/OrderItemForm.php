f444dc1369fa76810c5494f923dc8a7808a006f9
Chijindu Nwokeohuru <chijindu.nwokeohuru@gmail.com>
2026-02-06 10:50:05 +0100

    feat: Configure OrderItemForm schema to display in a single column.

--- app/Filament/Resources/OrderItems/Schemas/OrderItemForm.php
+++ app/Filament/Resources/OrderItems/Schemas/OrderItemForm.php
@@ -9,6 +9,7 @@ class OrderItemForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(1)
             ->components([
                 \Filament\Schemas\Components\Section::make('Item Details')
                     ->schema([
